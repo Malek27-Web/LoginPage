508965f9e77fa3d45667386c46dd1dcf878bb659
Malek Belghith <malikbelghith@gmail.com>
2024-04-29 13:53:51 +0200

    😁 Style Popup Changes

--- src/Components/index/Popup.tsx
+++ src/Components/index/Popup.tsx
@@ -5,11 +5,12 @@ interface Props {
 }
 function Popup({ textPrpos, onClose }: Props) {
   return (
-    <div className="border d-flex flex-column  align-items-center justify-content-center  border-1 border-dark w-25 h-25 d-inline-block  popup">
-      <p className="fs-3 fw-medium "> {textPrpos}</p>
-      <button className="btn btn-secondary" onClick={onClose}>
-        close
-      </button>
+    <div className="border d-flex flex-column   align-items-center justify-content-center  w-25 h-25 d-inline-block  popup">
+      <p className="fs-1 fw-medium text-light"> {textPrpos}</p>
+      <button
+        className="btn btn-close btn-close-white position-absolute button"
+        onClick={onClose}
+      ></button>
     </div>
   );
 }
